f051a11dcbb8eafa35cbabf27af40bf6d5d08535
Christian Lalanne <christian.lalanne@ichec.ie>
2014-07-31 01:25:45 +0100

    adding benchmark scripts

---
+++ bench.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for wgSize in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096; do
+    echo "Working Group Size ${wgSize}"
+    for i in {1..5}; do
+        echo "execution number ${i}"
+        ./mxm more_work.cl ${wgSize} >> wg_${wgSize}.dat
+    done
+done
