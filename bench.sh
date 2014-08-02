86d7f30865b56fbba0e9725f6fae975e52bdac41
Christian Lalanne <christian.lalanne@ichec.ie>
2014-08-02 20:38:11 +0100

    fixing

--- bench.sh
+++ bench.sh
@@ -4,6 +4,6 @@ for wgSize in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096; do
     echo "Working Group Size ${wgSize}"
     for i in {1..5}; do
         echo "execution number ${i}"
-        ./mxm private_local_memory.cl ${wgSize} >> wg_${wgSize}_phi.dat
+        ./mxm block.cl ${wgSize} >> wg_${wgSize}_phi.dat
     done
 done
