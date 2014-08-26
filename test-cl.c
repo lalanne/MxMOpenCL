3034aa44ded0a5a6b2211c1d4cb2248558cac48f
Christian Lalanne <christian.lalanne@ichec.ie>
2014-08-26 01:25:16 +0100

    device

--- test-cl.c
+++ test-cl.c
@@ -100,7 +100,7 @@ int main(int argc, char** argv){
 #endif
 
     err = clGetDeviceIDs(platform_id, 
-                        fpga ? CL_DEVICE_TYPE_ACCELERATOR : CL_DEVICE_TYPE_GPU/*CL_DEVICE_TYPE_CPU*//*CL_DEVICE_TYPE_ACCELERATOR*/,
+                        fpga ? CL_DEVICE_TYPE_ACCELERATOR : /*CL_DEVICE_TYPE_GPU*//*CL_DEVICE_TYPE_CPU*/CL_DEVICE_TYPE_ACCELERATOR,
                         1, 
                         &device_id, 
                         NULL);
