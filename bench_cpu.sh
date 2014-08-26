8407c25c26ad9b026f368d6575539c33b2a1bfda
Christian Lalanne <christian.lalanne@ichec.ie>
2014-08-26 11:10:39 +0100

    cpu

--- bench_cpu.sh
+++ bench_cpu.sh
@@ -4,6 +4,6 @@ for wgSize in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096; do
     echo "Working Group Size ${wgSize}"
     for i in {1..5}; do
         echo "execution number ${i}"
-        ./mxm more_work.cl ${wgSize} >> wg_${wgSize}.dat
+        ./mxm more_work.cl ${wgSize} >> wg_${wgSize}_cpu.dat
     done
 done
